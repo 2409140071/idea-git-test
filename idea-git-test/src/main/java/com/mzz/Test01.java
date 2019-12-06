df2d7b5fdfa6c2b1818f6398e99be49e32288a0b
2409140071 <2409140071@qq.com>
2019-12-06 22:36:43 +0800

    首次把本地项目添加到本地仓库

---
+++ idea-git-test/src/main/java/com/mzz/Test01.java
@@ -0,0 +1,12 @@
+package com.mzz;
+
+/**
+ * @Author MZZ
+ * @Date 2019/12/6/0006 22:28
+ * @Version 1.0
+ **/
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一个git测试项目");
+    }
+}
