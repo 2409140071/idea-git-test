8ef61a33ec5bd4e4306e46289c2e16dd9fd20f8f
2409140071 <2409140071@qq.com>
2019-12-06 22:38:36 +0800

    第二次把本地项目添加到本地仓库

---
+++ idea-git-test/src/main/java/com/mzz/Test2.java
@@ -0,0 +1,12 @@
+package com.mzz;
+
+/**
+ * @Author MZZ
+ * @Date 2019/12/6/0006 22:37
+ * @Version 1.0
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个文件");
+    }
+}
